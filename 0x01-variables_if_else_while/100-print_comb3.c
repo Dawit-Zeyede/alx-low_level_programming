529cb93757d012fe765daac8ef866b5358c11978
Dawit-Zeyede <devazeyede10@gmail.com>
2023-07-14 11:35:46 -0700

    possible combination

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,12 +13,15 @@ int main(void)
 	{
 		for (j = 48; j < 58; j++)
 		{
-			putchar(i);
-			putchar(j);
-			if ((i != 57) | (j!= 57))
+			if (i < j)
 			{
-				putchar(',');
-				putchar(' ');
+				putchar(i);
+				putchar(j);
+				if ((i != 56) | (j!= 57))
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
