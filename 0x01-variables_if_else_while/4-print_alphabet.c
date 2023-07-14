49555319c9eb43979d1df00ce3d102b072bd2038
Dawit-Zeyede <devazeyede10@gmail.com>
2023-07-14 09:43:40 -0700

    print alphabet with exeptions

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints alphabet in lower case except q and e.
+ *
+ * Return: ends the program.
+ */
+int main(void)
+{
+	char alpha[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+		if (alpha[i] != 'e' || alpha[i] != 'a')
+			putchar(alpha[i]);
+	putchar('\n');
+	return (0);
+}
