ad431b263f229032a62f402080f8f6d89c6ba77a
Dawit-Zeyede <devazeyede10@gmail.com>
2023-07-14 10:03:37 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints alphabet in lower case except q and e.
+ *
+ * Return: ends the program.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	putchar('\n');
+	return (0);
+}
