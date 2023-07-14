55be1b7d1c8fbf5e05e1eaefb1320bfc57148a7d
Dawit-Zeyede <devazeyede10@gmail.com>
2023-07-14 12:09:31 -0700

    possible combination of three digit numbers

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,21 +10,21 @@ int main(void)
 	int i;
 	int j;
 	int m;
-	
+
 	for (i = 48; i < 58; i++)
 	{
-		for (j=49 ; j < 58; j++)
+		for (j = 49 ; j < 58; j++)
 		{
 			if (i < j)
 			{
-				for(m=50; m<58 ; m++)
+				for (m = 50; m < 58 ; m++)
 				{
 					if (j < m)
 					{
 						putchar(i);
 						putchar(j);
-						putchar(m);	
-						if ((i != 55) | (j != 56) |( m != 57))
+						putchar(m);
+						if ((i != 55) | (j != 56) | (m != 57))
 						{
 							putchar(',');
 							putchar(' ');
