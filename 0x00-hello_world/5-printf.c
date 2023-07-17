f9dd5afe279a93d4d400c7f51d94dff10012bec2
Dawit-Zeyede <devazeyede10@gmail.com>
2023-07-17 09:37:52 -0700

    printing a statement using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+  * main - will display a given statements.
+  *
+  * Return: end of the program.
+  */
+
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
