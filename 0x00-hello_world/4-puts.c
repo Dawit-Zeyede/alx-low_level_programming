79902cfa25c62739ed651a1a36e6656ce87877ec
Dawit-Zeyede <devazeyede10@gmail.com>
2023-07-17 09:25:37 -0700

    printing string using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+  * main - will display a given statements.
+  *
+  * Return: end of the program.
+  */
+
+int main(void){
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return 0;
+}
