631ffa6406d803e2cbf2c2322701fb6b878ebeb7
Dawit-Zeyede <devazeyede10@gmail.com>
2023-07-18 06:42:43 -0700

    print alphabet in lower case

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,11 @@
 #include <stdio.h>
+#include "main.h"
 /**
  * main - prints alphabet in lower case.
  *
  * Return: ends the program.
  */
-int main(void)
+int main (void)
 {
-	char alpha[26] = "abcdefghijklmnopqrstuvwxyz";
-	int i;
-
-	for (i = 0; i < 26; i++)
-		putchar(alpha[i]);
-	putchar('\n');
-	return (0);
+	cha();
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,12 @@
 #ifndef main
 #define main
-
+void cha()
+  {
+          char alpha[26] = "abcdefghijklmnopqrstuvwxyz";
+          int i;
+  
+           for (i = 0; i < 26; i++)
+                 putchar(alpha[i]);
+          putchar('\n');
+  }
 #endif
