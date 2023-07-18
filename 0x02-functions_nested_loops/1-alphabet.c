e5173846d31091004d1e3a06d123948fb4177dce
Dawit-Zeyede <devazeyede10@gmail.com>
2023-07-18 09:35:27 -0700

    lower case alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -9,9 +9,9 @@ void print_alphabet(void)
 {
 
 	char alpha[26] = "abcdefghijklmnopqrstuvwxyz";
-        int i;
- 
-         for (i = 0; i < 26; i++)
-                  _putchar(alpha[i]);
-         _putchar('\n');
+	int i;
+
+	for (i = 0; i < 26; i++)
+		_putchar(alpha[i]);
+	_putchar('\n');
 }
