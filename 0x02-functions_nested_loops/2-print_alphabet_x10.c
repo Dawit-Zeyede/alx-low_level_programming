afebcb0e1eb2fe2f1e02044fe62a29abcada0b29
Dawit-Zeyede <devazeyede10@gmail.com>
2023-07-18 09:59:45 -0700

    10x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,7 +5,7 @@
   * Return: void
   */
 
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
 
 	char alpha[26] = "abcdefghijklmnopqrstuvwxyz";
@@ -15,5 +15,5 @@ void print_alphabet(void)
 	for (i = 0; i < 10; i++)
 		for (j = 0; j < 25; j++)
 			_putchar(alpha[j]);
-	_putchar("\n")
+	_putchar('\n');
 }
