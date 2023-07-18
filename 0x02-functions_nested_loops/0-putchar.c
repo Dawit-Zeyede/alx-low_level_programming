4f57cc872816f060f720ad0dfd67e51c6bc772f9
Dawit-Zeyede <devazeyede10@gmail.com>
2023-07-18 08:52:31 -0700

    put char

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,13 @@
-#include <stdio.h>
+#include "main.h"
+
 /**
-  * main - prints _putchar
-  *
-  * Return: end of the file
-  */
-
-int main(void)
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
 {
-	int i = 0;
-	char cha[] = "_putchar";
-
-	for (i = 0; i < 9; i++)
-	{
-		putchar(cha[i]);
-	}
-	putchar('\n');
-	return (0);
+	return (write(1, &c, 1));
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,12 +1,5 @@
-#ifndef main
-#define main
-void cha()
-  {
-          char alpha[26] = "abcdefghijklmnopqrstuvwxyz";
-          int i;
-  
-           for (i = 0; i < 26; i++)
-                 putchar(alpha[i]);
-          putchar('\n');
-  }
+#ifndef MAIN
+#define MAIN
+int _putchar(char c);
+
 #endif
