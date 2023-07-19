78a9b311cf5d920bf339d0d299d1ceb69be930d5
Dawit-Zeyede <devazeyede10@gmail.com>
2023-07-19 10:16:54 -0700

    fibonacci sequence

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+  * main - print the first 50 fibonacci sequence with out the first element.
+  *
+  * Return: end of the program.
+  */
+
+int main(void)
+{
+	int i;
+	long int n1;
+	long int n2;
+	long int next;
+
+	n1 = 1;
+	n2 = 2;
+	printf("%ld ,%ld ,", n1, n2);
+	for (i = 1; i < 97; i++)
+	{
+		next = n1 + n2;
+		if (i != 48)
+		{
+			printf("%ld, ", next);
+			n1 = n2;
+			n2 = next;
+		}
+		else
+			printf("%ld\n", next);
+	}
+	return (0);
+}
+
