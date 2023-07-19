fbfea53a3dc26347ebe9e4ce903e2bab95598a07
Dawit-Zeyede <devazeyede10@gmail.com>
2023-07-19 10:40:44 -0700

    24-hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,36 +1,27 @@
 #include "main.h"
 /**
-  * jack_bauer - prints all combinations
-  *
-  * Return: end of the program
+  * jack_bauer - prints 24-hours
+  * 
   */
 void jack_bauer(void)
 {
 	int i;
 	int j;
-	int m;
-	int n;
 
-	for (i = 48; i < 51; i++)
+	i = 0;
+	while(i < 24)
 	{
-		for (j = 48; j < 58; j++)
+		j = 0;
+		while(j < 60)
 		{
-			for (m = 48; m < 54; m++)
-			{
-				for (n = 48; n < 58; n++)
-				{
-					_putchar(i);
-					_putchar(j);
-					_putchar(':');
-					_putchar(m);
-					_putchar(n);
-					_putchar('\n');
-					if ((i == 50 && j == 51) && (m == 53 && n == 59))
-					{
-						break;
-					}
-				}
-			}
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			_putchar(':');
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+			_putchar('\n');
+			j++;
 		}
+		i++;
 	}
 }
