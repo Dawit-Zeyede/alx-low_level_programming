5fd431fce99a1c35511d6b6bf6b1564b24c8aa07
Dawit-Zeyede <devazeyede10@gmail.com>
2023-07-20 05:52:15 -0700

    check for upper case letters

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,13 +3,12 @@
 /**
   * _isupper - check for upper letters.
   * @c - the character to be checked.
-  * Return: end of the program.
+  *Return: end of the program.
   */
 
 int _isupper(int c)
 {
 	if (c >= 65 && c <= 90)
 		return (1);
-	else
-		return (0);
+	return (0);
 }
