e5f3e7902168e6a6f0440a66e91fd4cfc126116e
Dawit-Zeyede <devazeyede10@gmail.com>
2023-07-24 10:37:17 -0700

    modifier

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+  * reset_to_98 - modify the variable outside of the functions.
+  * @n: pointer containg address for the variable being modified.
+  * Return: end of the program.
+  */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+this is a readme for pointer and string
