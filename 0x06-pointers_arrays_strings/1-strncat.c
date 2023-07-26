e15a0163ac69b04bef9deb736ac912569269bf5e
Dawit-Zeyede <devazeyede10@gmail.com>
2023-07-26 10:32:31 -0700

    concatiate up to n

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,24 +1,21 @@
 #include "main.h"
 /**
-  * _strncat - concatinate two strings
-  * @dest: address of the first string
-  * @src: address of the second string
+  * _strncat - concatinate up to n.
+  * @dest: string appendent
+  * @src: string to be appended
+  * @n: no of bytes to be appended
   * Return: end of the program
   */
 char *_strncat(char *dest, char *src, int n)
 {
-	int dest_len;
 	int i;
+	int len;
 
-	dest_len = 0;
-	while (dest[dest_len] != '\0')
-	{
-		dest_len += 1;
-	}
+	len = 0;
+	while (dest[len] != '\0')
+		len += 1;
 	for (i = 0; i < n && src[i] != '\0'; i++)
-	{
-		dest[dest_len + i] = src[i];
-	}
-	dest[dest_len + i] = '\0';
+		dest[len + i] = src[i];
+	dest[len + i] = '\0';
 	return (dest);
 }
