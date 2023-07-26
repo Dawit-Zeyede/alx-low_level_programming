41a73bad060360d0c8b09ae6ad39b8aaad062205
Dawit-Zeyede <devazeyede10@gmail.com>
2023-07-26 04:38:04 -0700

    every two characters

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,10 +6,16 @@
   */
 void puts2(char *str)
 {
-	while (*str != '\0')
+	int len;
+	int i;
+
+	len = 0;
+	while (str[len] != '\0')
 	{
-		_putchar(*str);
-		str += 2;
+		len += 1;
 	}
-	_putchar('\n');
+	for (i = 0; i < len; i += 2)
+	{
+		_putchar(str[i]);
+	}
 }
