2186549d38decc55afcd318d3215ea596a80ce19
Dawit-Zeyede <devazeyede10@gmail.com>
2023-08-03 11:27:46 -0700

    palindrome checker

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,4 +1,6 @@
 #include "main.h"
+int checker(char *s, int i, int len);
+int counter(char *s);
 /**
   * is_palindrome - check for empty string.
   * @s: stirng address to be checked.
@@ -6,15 +8,33 @@
   */
 int is_palindrome(char *s)
 {
-	int  len;
-
-	len = 0;
-	while (*s != '\0')
-	{
-		len += 1;
-		len += is_palindrome(s + 1);
-	}
-	if (len == 0)
+	if (*s == 0)
 		return (1);
-	return (0);
+	return (checker(s, 0, counter(s)));
+}
+/**
+  * counter - counts the string.
+  * @s: string to be counted.
+  * Return: end of the program.
+  */
+int counter(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + counter(s + 1));
+}
+/**
+  * checker - palindrome checker.
+  * @s: string to be cheked.
+  * @i: iterator.
+  * @len: length of a string.
+  * Return: end of the program.
+  */
+int checker(char *s, int i, int len)
+{
+	if (*(s + i) != *(s + len - 1))
+		return (0);
+	if (i >= len)
+		return (1);
+	return (checker(s, i + 1, len - 1));
 }
