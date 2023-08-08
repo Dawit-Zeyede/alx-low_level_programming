9a00ab2fdb5d770249e579e64d7d189f3a0740df
Dawit-Zeyede <devazeyede10@gmail.com>
2023-08-08 10:22:27 -0700

    reallocate and copy

--- 0x0B-malloc_free/1-main.c
+++ 0x0B-malloc_free/1-main.c
@@ -11,7 +11,7 @@ int main(void)
 {
     char *s;
 
-    s = _strdup("First, solve the problem. then write the code");
+    s = _strdup(NULL);
     if (s == NULL)
     {
         printf("failed to allocate memory\n");
