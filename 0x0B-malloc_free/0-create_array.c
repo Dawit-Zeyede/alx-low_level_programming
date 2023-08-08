e11034af37a82b7480f5fc279983f7edcd0ad4a4
Dawit-Zeyede <devazeyede10@gmail.com>
2023-08-08 03:59:18 -0700

    unspecified array using malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,6 +13,10 @@ char *create_array(unsigned int size, char c)
 		char *str;
 
 		str = malloc(sizeof(char) * size);
+		if (str == NULL)
+		{
+			return (NULL);
+		}
 		str[0] = c;
 		return str;
 	}
