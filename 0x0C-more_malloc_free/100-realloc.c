9953c1db8c49c7341cad2616b35a7e8b9e434dac
Dawit-Zeyede <devazeyede10@gmail.com>
2023-08-10 11:24:38 -0700

    reallocate

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -40,7 +40,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		for (i = 0; i < new_size; i++)
 			new[i] = old[i];
 	}
-	new[new_size] = '\0';
 	free(ptr);
 	return (new);
 }
