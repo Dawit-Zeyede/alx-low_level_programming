d55a78bb87b23719871655a8e3fca4b947e6c949
Dawit-Zeyede <devazeyede10@gmail.com>
2023-08-10 03:39:59 -0700

    allocate a desired space

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+  * malloc_checked - allocates a desired amount of space.
+  * @n: amount to be allocated.
+  * Return: ends with 98 for failure.
+  */
+void *malloc_checked(unsigned int n)
+{
+	void *str;
+	str = malloc(n);
+
+	if (str == NULL)
+		exit (98);
+	return (str);
+}
