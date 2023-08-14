463bc95aaf59febe35b8d916e875f9d2d9042a6f
Dawit-Zeyede <devazeyede10@gmail.com>
2023-08-14 05:09:13 -0700

    Initializing DOG

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+/**
+  * init_dog - intializes struct dog varible type.
+  * @d: address of variable to be intialized.
+  * @name: first value to be assigned.
+  * @age: second value to be assigned.
+  * @owner: thrid value to be assigned.
+  */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
