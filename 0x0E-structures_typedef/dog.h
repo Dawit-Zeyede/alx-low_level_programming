3d89c7f4bfb04aab228bb77ac6b9d5a48fb1ee61
Dawit-Zeyede <devazeyede10@gmail.com>
2023-08-14 04:57:04 -0700

    DOG

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
-struct dog{
+/**
+  * struct dog - Dog details
+  * @name: pointer towards the name of dog.
+  * @age: age of the dog.
+  * @owner: owner of the dog.
+  *
+  * This is a header file for data type called dog.
+  */
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
