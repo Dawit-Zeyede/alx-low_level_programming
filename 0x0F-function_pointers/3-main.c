53ead1a1e8f7e026f7c69af0e1e0bb90d0042dcd
Dawit-Zeyede <devazeyede10@gmail.com>
2023-08-16 11:31:02 -0700

    OPERATOR

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,36 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  * main - test your program.
+  * @argc: number of argument during run time.
+  * @argv: array of arguments.
+  * Return: end of the program.
+  */
+int main(int argc, char *argv[])
+{
+	int a;
+	int b;
+	char *opr;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	opr = argv[2];
+	if (get_op_func(opr) == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((*opr == '/' && b == 0) || (*opr == '%' && b == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", get_op_func(opr)(a, b));
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "3-calc.h"
+/**
+  * op_add - adder.
+  * @a: int 1.
+  * @b: int 2.
+  * Return: end of the program.
+  */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+  * op_sub - subtractor.
+  * @a: int 1.
+  * @b: int 2.
+  * Return: end of the program.
+  */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+  * op_mul - multiplier.
+  * @a: int 1;
+  * @b: int 2
+  * Return: end of the program
+  */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+  * op_div - divider.
+  * @a: int 1;
+  * @b: int 2
+  * Return: end of the program
+  */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+  * op_mod - moduler.
+  * @a: int 1
+  * @b: int 2
+  * Return: end of the program.
+  */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
