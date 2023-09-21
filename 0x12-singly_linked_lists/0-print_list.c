3d5b7b6ca030d94e64032a4f48b3ea8c1c9e30ec
Dawit-Zeyede <devazeyede10@gmail.com>
2023-09-21 11:49:10 -0700

    Singly linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -20,7 +20,7 @@ size_t print_list(const list_t *h)
 			printf("[0] (nill)\n");
 		else
 		{
-			printf("[%d] %s\n", h->, h->str));
+			printf("[%d] %s\n", h->len, h->str);
 		}
 		h = h->next;
 		count += 1;
