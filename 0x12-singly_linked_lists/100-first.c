74b21e6eea206f74e6cf1a80c48c3e4eb40248dc
Dawit-Zeyede <devazeyede10@gmail.com>
2023-09-21 12:09:45 -0700

    Singly linked list

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+  * _print - executed before main function.
+  * description: constructor and deconstructor.
+  */
+void _print(void)
+{
+	printf("You're beat! and yet, you must allow,");
+	printf("\nI bore my house upon my back!\n");
+}
