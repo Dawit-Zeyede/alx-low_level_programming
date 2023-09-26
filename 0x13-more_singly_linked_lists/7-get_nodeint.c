17f4b3838a5d1914d99e6a4d74a2eeeb0a62d47d
Dawit-Zeyede <devazeyede10@gmail.com>
2023-09-26 09:45:54 -0700

    Singly linked  list

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+  * get_nodeint_atindex - get the address of a node at a given index.
+  * @head: address of the first node.
+  * @index: index of a  node to be returned.
+  * Return: address of the indexede node.
+  */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *first;
+	unsigned int i;
+
+	first = head;
+	for (i = 0; i < index; i++)
+	{
+		if (first->next != NULL)
+			first = first->next;
+		else
+			return (NULL);
+	}
+	return (first);
+}
