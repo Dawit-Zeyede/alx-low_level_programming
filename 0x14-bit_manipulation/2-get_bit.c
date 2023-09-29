0ff1c97b83f191ac1e6957a13bdb32cb5282cde9
Dawit-Zeyede <devazeyede10@gmail.com>
2023-09-29 11:20:59 -0700

    Get bit

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+  * git_bit - value of a bit at a specific index.
+  * @n: number to be positioned.
+  * @index: position of a bit.
+  * Return: end to the program.
+  */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int i;
+	int at_bit;
+
+	i = 0;
+	if (index > (sizeof(int) * 8))
+		return (-1);
+	while (i < index)
+		n = n >> 1;
+	at_bit= n & 1;
+	return (at_bit);
+}
