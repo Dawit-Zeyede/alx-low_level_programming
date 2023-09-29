403d54f2eb427df5e8843b4b8a6f3393a0850e3f
Dawit-Zeyede <devazeyede10@gmail.com>
2023-09-29 10:56:30 -0700

    Endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+  * get_endianness - checks for network byte order.
+  * Return: Big endian or Little endian
+  */
+int get_endianness(void)
+{
+	int n;
+	char *c;
+
+	n = 1;
+	c = (char*) &n;
+	return (int)*c;
+}
