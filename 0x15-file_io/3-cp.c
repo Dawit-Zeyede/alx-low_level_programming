9052f2086a682b9b3634e3f9f5054adab788df09
Dawit-Zeyede <devazeyede10@gmail.com>
2023-10-06 20:09:56 +0300

    file handle

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -4,7 +4,6 @@
  * @file_from: file_from
  * @file_to: file_to
  * @argv: arguments vector.
- * Return: no return.
  */
 void error_file(int file_from, int file_to, char *argv[])
 {
@@ -55,13 +54,13 @@ int main(int argc, char *argv[])
 	err_close = close(file_from);
 	if (err_close == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: can't close fd %d\n", file_from);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(100);
 	}
 	err_close = close(file_to);
 	if (err_close == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: can't close fd %d\n", file_from);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(100);
 	}
 	return (0);
