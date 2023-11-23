a097f88bca6b714dbfefce7c713300a003cdeebf
Dawit-Zeyede <devazeyede10@gmail.com>
2023-11-23 22:21:16 +0300

    bit

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * printer - prints 0's and 1's
+ * @value: Decimal number
+ */
+void printer(unsigned long int n)
+{
+	if (n == 0)
+		return;
+	printer(n >> 1);
+	if (n & 1 == 1)
+		_putchar('1');
+	else
+		_putchar('0');
+}
+/** print_binary - printer
+ * @n: Decimal number
+ */
+void print_binary(unsigned long int n)
+{
+	if (n == 0)
+		_putchar('0');
+	else
+		printer(n);
+}
