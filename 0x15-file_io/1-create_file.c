83c28febfaa1ee6476b7b9a4e144d6e08a4a5c4f
Dawit-Zeyede <devazeyede10@gmail.com>
2023-11-27 21:44:20 +0300

    file i/o

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * create_file - a function to create a file.
+ * @filename: a name for a new file.
+ * @text_content: a NULL terminated string to write to the file.
+ * Return: End of the program.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int i;
+	int fd;
+	int written;
+	int count = 0;
+
+	if (filename == NULL)
+		return (-1);
+	for (i = 0; text_content[i] != '\0'; i++)
+		count += 1;
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd < 0)
+		return (-1);
+	if (text_content == NULL)
+		return (-1);
+	written = write(fd, text_content, count);
+	close(fd);
+	return (1);
+}
