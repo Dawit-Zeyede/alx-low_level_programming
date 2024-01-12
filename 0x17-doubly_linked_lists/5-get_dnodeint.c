c7d128b3362532da411f8b9016d54b3121d9c85e
Dawit-Zeyede <devazeyede10@gmail.com>
2024-01-12 16:10:59 +0300

    5-get

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -10,12 +10,12 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	unsigned int n;
 
 	if (head == NULL)
-		return NULL;
+		return (NULL);
 	n = 0;
 	while (head)
 	{
 		if (n == index)
-			return head;
+			return (head);
 		head = head->next;
 		n += 1;
 	}
